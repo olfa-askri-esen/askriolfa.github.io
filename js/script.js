b07a616afd7de7e5adcb0a4410578fe7fd5bcb18
askri olfa <olfa.askri@esen.tn>
2023-03-14 14:55:00 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+    // enveloppe 
+    $(document)
+    .ready(function() {
+        $('.frame')
+            .click(function() {
+                $('.top')
+                    .addClass('open');
+                $('.message')
+                    .addClass('pull');
+            })
+    });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
